82ae613c36a6078075f5dd96d70882cde9614165
ksuryaprakash777-ux <k.suryaprakash777@gmail.com>
2026-05-23 17:57:46 +0530

    fixed loginUser export

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,6 +4,6 @@ const API = axios.create({
   baseURL: 'https://workforce-api-fxeq.onrender.com/api'
 });
 
-export const login = async (data) => {
+export const loginUser = async (data) => {
   return API.post('/auth/login', data);
 };
